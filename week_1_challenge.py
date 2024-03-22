78200c20e8d361e248679696b9105f9e1cec597a
Willy-Angole <erotangole@gmail.com>
2024-03-22 23:46:47 +0300

    challenge submission

---
+++ week_1_challenge.py
@@ -0,0 +1,9 @@
+#Weekly code challenge
+
+name = input("Enter your Name: ")
+age = int(input("Enter your age: "))
+location = input("Enter your location: ")
+
+# #print name, age and location of the user
+print("Hello {} you are {} years old and you live in {}" .format(name, age, location))
+
